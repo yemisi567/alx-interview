42d5745621e6a3dbeb3df3f3ed0554ce6c37cd6a
Alegbe Mojisola Yemisi <51059680+yemisi567@users.noreply.github.com>
2023-02-10 11:58:25 +0100

    fix beneficiray

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -26,7 +26,7 @@ async function charsInFilm(urlList) {
   }
 }
 
-const filmsURL = "https://swapi-api.hbtn.io/api/films/" + process.argv[2];
+const filmsURL = "https://swapi-api.alx-tools.com/api/films/" + process.argv[2];
 request(filmsURL, function (error, response, body) {
   if (error) {
     console.error(error);
